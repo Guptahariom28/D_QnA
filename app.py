d85bee6b739dfd4c6e00586de43b3e3892490381
Guptahariom28 <91150819+Guptahariom28@users.noreply.github.com>
2024-07-26 15:31:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -26,23 +27,23 @@ huggingface_embedding= HuggingFaceEmbeddings(
     encode_kwargs={'normalize_embeddings':True}
 )
 
-prompt= ChatPromptTemplate.from_template(
-    """
-    I want you to act as a "Condition Monitoring Engineer".
-    please provide the ansert on the basis of the context only.
-    The answer should be accurate and from the context.
+prompt=ChatPromptTemplate.from_template(
+"""
+ want you to act as a "Condition Monitoring Engineer".
+Please provide the answer based on the context only.
+The answer should be accurate and from the context.
+<context>
+{context}
+<context>
+Questions:{input}
 
-    <context>
-    {context}
-    <context>
-    Question: {question}
-    """
+"""
 )
 
 def vector_embedding():
     if 'vectors' not in st.session_state:
         st.session_state.embeddings= huggingface_embedding
-        st.session_state.loader= PyPDFDirectoryLoader("data\\")
+        st.session_state.loader= PyPDFDirectoryLoader("data//")
         st.session_state.docs= st.session_state.loader.load()
         st.session_state.text_splitter= RecursiveCharacterTextSplitter(chunk_size= 1000, chunk_overlap=200)
         st.session_state.final_doc= st.session_state.text_splitter.split_documents(st.session_state.docs)
@@ -57,11 +58,11 @@ prompt1= st.text_input("Enter the Question from the Document")
 
 if prompt1:
     
-    document_chain= create_stuff_documents_chain(llm, prompt)
-    retriever= st.session_state.vectors.as_retriever(search_type="similarity",search_kwargs={"k":3})
-    retrieval_chain= create_retrieval_chain(retriever, document_chain)
-    responde= retrieval_chain.run('input', prompt1)
-    st.write(responde['answer'])
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+    response=retrieval_chain.invoke({'input':prompt1})
+    st.write(response['answer'])
     with st.expander("Document Similarity Search"):
         for i, doc in enumerate(response["context"]):
             st.write(doc.page_content)
